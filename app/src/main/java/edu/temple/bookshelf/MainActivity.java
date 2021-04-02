8f69bc0e67fb2258b54edbe5b745e818cc5212bd
Sen <tug81999@temple.edu>
2021-04-02 18:12:16 -0400

    added professor's solution

--- app/src/main/java/edu/temple/bookshelf/MainActivity.java
+++ app/src/main/java/edu/temple/bookshelf/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.temple.bookshelf;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
@@ -11,50 +13,94 @@ public class MainActivity extends AppCompatActivity implements ListFragment.List
     BookList bookList;
     DisplayFragment displayFragment;
     boolean secondContainer;
-    boolean itemChosen;
+    Book selectedBook;
     FragmentManager manager;
+    private final String ARG_SELECTED_BOOK = "selectedBook";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            selectedBook = savedInstanceState.getParcelable(ARG_SELECTED_BOOK);
+        }
+
         manager = getSupportFragmentManager();
         secondContainer = findViewById(R.id.container_2) != null;
-
-        Log.d("ACTIVITY", "Launched main activity! Value of itemChosen: ");
 
         bookList = new BookList(this);
         bookList.readFromFile("bookfile");
 
-        manager
-                .beginTransaction()
-                .replace(R.id.container_1, ListFragment.newInstance(bookList))
-                .commit();
+        Fragment f1;
+        f1 = manager.findFragmentById((R.id.container_1));
 
-
-        displayFragment = new DisplayFragment();
-        if(secondContainer) {
+        if (f1 instanceof DisplayFragment) {
+            manager.popBackStack();
+        } else if (!(f1 instanceof ListFragment)) {
             manager
                     .beginTransaction()
-                    .add(R.id.container_2, displayFragment)
+                    .add(R.id.container_1, ListFragment.newInstance(bookList), "BOOKLIST")
+                    .commit();
+        }
+
+        displayFragment = (selectedBook == null) ? new DisplayFragment() : DisplayFragment.newInstance(selectedBook);
+
+        if (secondContainer) {
+            manager
+                    .beginTransaction()
+                    .replace(R.id.container_2, displayFragment, "DISPLAY")
+                    .commit();
+        } else if (selectedBook != null) {
+            manager
+                    .beginTransaction()
+                    .replace(R.id.container_1, displayFragment, "DISPLAY")
                     .addToBackStack(null)
                     .commit();
         }
+
+//        manager
+//                .beginTransaction()
+//                .replace(R.id.container_1, ListFragment.newInstance(bookList))
+//                .commit();
+//
+//
+//        displayFragment = new DisplayFragment();
+//        if(secondContainer) {
+//            manager
+//                    .beginTransaction()
+//                    .add(R.id.container_2, displayFragment)
+//                    .addToBackStack(null)
+//                    .commit();
+//        }
 
     }
 
     @Override
     public void itemClicked(int position) {
+        selectedBook = bookList.get(position);
+
         if(secondContainer) {
             displayFragment.changeBook(bookList.get(position));
         } else {
-            displayFragment = DisplayFragment.newInstance(bookList.get(position));
+            displayFragment = DisplayFragment.newInstance(selectedBook);
             manager
                 .beginTransaction()
-                .replace(R.id.container_1, displayFragment)
+                .replace(R.id.container_1, displayFragment, "DISPLAY")
                 .addToBackStack(null)
                 .commit();
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(ARG_SELECTED_BOOK, selectedBook);
+    }
+
+    @Override
+    public void onBackPressed() {
+        selectedBook = null;
+        super.onBackPressed();
     }
 }
